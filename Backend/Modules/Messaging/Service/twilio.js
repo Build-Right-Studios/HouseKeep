bd6e57a7e8770ee423939e5330467b585898598f
Aryesh02 <aryeshsrivastava@gmail.com>
2026-03-07 17:54:27 +0530

    Setup Redis for Render

--- Backend/Modules/Messaging/Service/twilio.js
+++ Backend/Modules/Messaging/Service/twilio.js
@@ -4,20 +4,25 @@ import { TWILIO } from "../../../constants.js";
 
 dotenv.config();
 
+// const client = twilio(
+//   TWILIO.TWILIO_ACCOUNT_SID,
+//   TWILIO.TWILIO_AUTH_TOKEN,
+// );
+
 const client = twilio(
-  TWILIO.TWILIO_ACCOUNT_SID,
-  TWILIO.TWILIO_AUTH_TOKEN,
+  process.env.TWILIO_ACCOUNT_SID,
+  process.env.TWILIO_AUTH_TOKEN,
 );
 
 export const sendOTP = async (phone, otp) => {
   try {
 
-    console.log("TWILIO FROM:", TWILIO.TWILIO_PHONE_NUMBER);
+    console.log("TWILIO FROM:", process.env.TWILIO_PHONE_NUMBER);
     console.log("OTP SENDING TO:", phone);
 
     const message = await client.messages.create({
       body: `Your HouseKeep OTP is ${otp}`,
-      from: TWILIO.TWILIO_PHONE_NUMBER,
+      from: process.env.TWILIO_PHONE_NUMBER,
       to: phone
     });
 
