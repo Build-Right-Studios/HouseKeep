0cc079fc20d1555f2190e6b7494a342b280c0bdd
codewitharyan-35 <aryan35chauhan@gmail.com>
2026-03-07 16:27:30 +0530

    Mongodb Setup Done

--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import registerRoutes from "./routes.js";
+import connectMongoDB from "./config/mongodb.js"
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+
+await connectMongoDB();
 
 registerRoutes(app);
 
